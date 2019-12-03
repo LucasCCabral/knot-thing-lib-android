26a7ef18a03eca72515a65527b0a2c61c39f68b0
Lucas Cabral <lcc2@cesar.org.br>
2019-12-03 13:24:32 +0000

    Fix example application publish-data
    Fix publish-data message by changing the parameters to obey the KNoT
    protocol.
    
    The former message was malformed according to the KNoT protocol, as such
    when the connector received the message it throwed an error specifying
    that the value did not match the value type.

--- app/src/main/java/com/example/knot_thing_lib_android/MainActivity.kt
+++ app/src/main/java/com/example/knot_thing_lib_android/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.knot_thing.KNoTAMQPFactory
 import com.example.knot_thing.KNoTMessager
+import com.example.knot_thing.KNoTTypes.KNOT_TYPE_ID_SWITCH
+import com.example.knot_thing.KNoTTypes.KNOT_UNIT_NOT_APPLICABLE
+import com.example.knot_thing.KNoTTypes.KNOT_VALUE_TYPE_BOOL
 import com.example.knot_thing_lib_android.KNoTControlMessages.KNoTThingAuth
 import com.example.knot_thing_lib_android.KNoTControlMessages.KNoTThingRegister
 import com.example.knot_thing_lib_android.KNoTControlMessages.KNoTThingSchema
@@ -21,6 +24,12 @@ import org.jetbrains.anko.doAsync
 class MainActivity : AppCompatActivity() {
     val HOSTNAME = "192.168.31.61"
     val PORT_NUMBER = 5672
+    val THING_ID = "a74151d19de59cd3"
+    val THING_NAME = "pocophone-lucas"
+    val SENSOR_ID = 1
+    val KNoT_VALUE_TYPE = KNOT_VALUE_TYPE_BOOL
+    val KNoT_UNIT = KNOT_UNIT_NOT_APPLICABLE
+    val KNoT_TYPE_ID = KNOT_TYPE_ID_SWITCH
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,20 +38,24 @@ class MainActivity : AppCompatActivity() {
         lateinit var kNoTMessager : KNoTMessager
         val setKNoTMessager = { kNoTMessagerAux : KNoTMessager -> kNoTMessager = kNoTMessagerAux }
         KNoTAMQPFactory().getKNoTProtocolMessager(kNoTAMQP, setKNoTMessager)
-        val kNoTThingRegister = KNoTThingRegister("1h3kkx1gi13idqe", "pocophone-lucas")
-        val kNoTThingUnregister = KNoTThingUnregister("1h3kkx1gi13idqe")
-        val kNoTThingAuth = KNoTThingAuth("1h3kkx1gi13idqe", "ejfhwekhrui234huirh23uf")
-        val kNoTThingSchema = mutableListOf(KNoTThingSchema(1,
-            21,
-            21,
-            12,
-            "test"))
+        val kNoTThingRegister = KNoTThingRegister(THING_ID, THING_NAME)
+        val kNoTThingUnregister = KNoTThingUnregister(THING_ID)
+        val kNoTThingAuth = KNoTThingAuth(THING_ID, "ejfhwekhrui234huirh23uf")
+        // TODO: Add verification to check if unit, value_type and type_id respect the KNoT Protocol
+        val kNoTThingSchema = mutableListOf(KNoTThingSchema(SENSOR_ID,
+            KNoT_VALUE_TYPE,
+            KNoT_UNIT,
+            KNoT_TYPE_ID,
+            "test")
+        )
 
-        val kNoTThingUpdateSchema = KNoTThingUpdateSchema("1h3kkx1gi13idqe", kNoTThingSchema)
-        val knotData = mutableListOf(KNoTThingData(12,true),
-            KNoTThingData(2,false),
-            KNoTThingData(12,true))
-        val kNoTThingUpdateData = KNoTThingUpdateData("1p23jp12k3p12", knotData)
+        val kNoTThingUpdateSchema = KNoTThingUpdateSchema(THING_ID, kNoTThingSchema)
+        val knotData = mutableListOf(KNoTThingData(SENSOR_ID,true),
+            KNoTThingData(SENSOR_ID,false),
+            KNoTThingData(SENSOR_ID,true)
+        )
+
+        val kNoTThingUpdateData = KNoTThingUpdateData(THING_ID, knotData)
 
         register_button.setOnClickListener {
             doAsync { kNoTMessager.register(kNoTThingRegister) }
