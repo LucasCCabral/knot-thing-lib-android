6a7c4fdb6ef2b7ee08e06daaf0e1c8619477f8bd
Lucas Cabral <lcc2@cesar.org.br>
2019-12-03 13:10:34 +0000

    Fix queue names capitalization

--- knot-thing/src/main/java/com/example/knot_thing/KNoTAMQP.kt
+++ knot-thing/src/main/java/com/example/knot_thing/KNoTAMQP.kt
@@ -20,8 +20,8 @@ class KNoTAMQP(username : String, password : String, hostname : String, port : I
     val BINDING_KEY_SCHEMA_UPDATE = "schema.update"
     val BINDING_KEY_DATA_PUBLISH = "data.publish"
 
-    val QUEUE_NAME_FOG_IN = "fogin"
-    val QUEUE_NAME_FOG_OUT = "fogout"
+    val QUEUE_NAME_FOG_IN = "fogIn"
+    val QUEUE_NAME_FOG_OUT = "fogOut"
 
     val CONSUMER_NAME = "KNoTThingConsumer"
 
